c31b732f474f501fea2aed132f2697212a75ae42
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2023-01-10 12:46:14 +0000

    added publication component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainNav from "./components/MainNav";
 
 class App extends React.Component {
   render() {
     return (
       <>
         <div>Hello world</div>
+        <MainNav />
       </>
     );
   }
---
+++ src/components/MainNav.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class MainNav extends React.Component {
+  render() {
+    return (
+      <>
+        <nav>
+          <div>About</div>
+          <div>Comissions</div>
+          <div>Projects</div>
+          <div>Publications</div>
+          <div>Home</div>
+        </nav>
+      </>
+    );
+  }
+}
+
+export default MainNav;
