0ed1a0c43ecb6c33fab06b604b7defe039bdf72f
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2023-01-18 15:02:19 +0000

    adjusted styling

--- src/App.js
+++ src/App.js
@@ -35,29 +35,64 @@ class App extends React.Component {
     return (
       <>
         {this.state.nameBackground ? (
-          <motion.div className="name">
-            <span className="name-header">
-              <div id="text">
-                <div class="line">
-                  <p class="word">CLAUDIA</p>
-                  <p class="word">CANTARINI</p>
-                </div>
+          <>
+            <motion.div className="name">
+              <span className="name-header">
+                <div id="text">
+                  <div class="line">
+                    <p class="word">CLAUDIA</p>
+                    <p class="word">CANTARINI</p>
+                  </div>
 
-                <div class="line">
-                  <p class="word">PHOTOGRAPHER</p>
-                  <p class="word">&</p>
-                </div>
+                  <div class="line">
+                    <p class="word">PHOTOGRAPHER</p>
+                    <p class="word">&</p>
+                  </div>
 
-                <div class="line">
-                  <p class="word"></p>
-                </div>
+                  <div class="line">
+                    <p class="word"></p>
+                  </div>
 
-                <div class="line">
-                  <a class="word fancy">CLAUDIA@HOTMAIL.COM</a>
+                  <div class="line">
+                    <p class="word">CLAUDIA@HOTMAIL.COM</p>
+                  </div>
                 </div>
+              </span>
+            </motion.div>
+            <div className="contacts-container">
+              <div></div>
+
+              <div className="contact">
+                <a
+                  href="https://www.instagram.com/camomilehaze/?hl=en"
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{
+                    textDecoration: "none",
+                    color: "white",
+                  }}
+                >
+                  Instagram
+                </a>
               </div>
-            </span>
-          </motion.div>
+              <div className="contact">
+                <a
+                  href="https://github.com/Jacobbidmead"
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{
+                    textDecoration: "none",
+                    color: "white",
+                    cursor: "pointer",
+                  }}
+                >
+                  {" "}
+                  GitHub
+                </a>
+                <div></div>
+              </div>
+            </div>
+          </>
         ) : (
           <>
             <motion.div className="main-container"></motion.div>
