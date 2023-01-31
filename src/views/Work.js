53b0a3bb44295abe0d14c3f5a02e346d6f5e9500
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2023-01-31 13:57:13 +0000

    added initial layout for work page

--- src/views/Work.js
+++ src/views/Work.js
@@ -1,9 +1,32 @@
 import React from "react";
+import "../styles/Work.css";
 
 const Work = () => {
   return (
     <>
-      <div>Hello world</div>
+      <div className="work-container">
+        {/* empty div */}
+        <div></div>
+        {/* empty div */}
+        <div className="link-container">
+          <div className="work-nav first">
+            <img src="images/cat.jpeg" alt="" />
+          </div>
+        </div>
+        <div className="link-container">
+          <div className="work-nav middle">
+            <img src="images/cat.jpeg" alt="" />
+          </div>
+        </div>
+        <div className="link-container">
+          <div className="work-nav end">
+            <img src="images/cat.jpeg" alt="" />
+          </div>
+        </div>
+        {/* empty div */}
+        <div></div>
+        {/* empty div */}
+      </div>
     </>
   );
 };
