dda1539f3483a9ed7eca56650ecba8a5d7dc259d
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2023-02-01 13:32:19 +0000

    Added entry animation to work links

--- src/views/Work.js
+++ src/views/Work.js
@@ -1,6 +1,19 @@
 import React from "react";
 import "../styles/Work.css";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const textAnimate = {
+  offscreen: { y: 100 },
+  onscreen: {
+    y: 0,
+    transition: {
+      type: "tween",
+      bounce: 0.5,
+      duration: 1,
+    },
+  },
+};
 
 const Work = () => {
   return (
@@ -11,24 +24,43 @@ const Work = () => {
         {/* empty div */}
         <div className="link-container">
           <Link to="/Projects" className="work-nav end">
-            <img src="images/cat.jpeg" alt="" className="image" />
+            <motion.img
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.1 }}
+              src="images/cat.jpeg"
+              alt=""
+              className="image"
+            />
             <span className="hidden-word projects-word">Projects</span>
           </Link>
         </div>
         <div className="link-container">
           <Link to="/Commissions" className="work-nav middle">
-            <img src="images/cat.jpeg" alt="" className="image" />
+            <motion.img
+              src="images/cat.jpeg"
+              alt=""
+              className="image"
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.1 }}
+            />
             <span className="hidden-word commissions-word">Commissions</span>
           </Link>
         </div>
         <div className="link-container">
           <Link to="/Publications" className="work-nav end">
-            <img src="images/cat.jpeg" alt="" className="image" />
+            <motion.img
+              src="images/cat.jpeg"
+              alt=""
+              className="image"
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.1 }}
+            />
             <span className="hidden-word publications-word">Publications</span>
           </Link>
         </div>
-
-        <div style={{ color: "white" }}>Home</div>
+        <Link className="home-button" to="/" style={{ color: "white" }}>
+          Home
+        </Link>
       </div>
     </>
   );
