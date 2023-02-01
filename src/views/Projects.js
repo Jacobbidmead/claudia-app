070c087beb877cec208e67d654bf918a1018a3b2
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2023-02-01 19:10:34 +0000

    Created main layout for projects view

--- src/views/Projects.js
+++ src/views/Projects.js
@@ -1,12 +1,30 @@
 import React from "react";
 import "../styles/Projects.css";
 
-const Projects = () => {
-  return (
-    <>
-      <div className="projects-container"></div>
-    </>
-  );
-};
+class Projects extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="projects-container">
+          <div className="content-nav">
+            <div className="content-links">link1</div>
+            <div className="content-links">link2</div>
+            <div className="content-links">link3</div>
+            <div className="content-links">link3</div>
+          </div>
+          <div className="content-image">
+            <div className="large-photo-container">
+              <img src="images/test.jpeg" alt="" className="main-photo" />
+            </div>
+          </div>
+          {/* empty div */}
+          <div></div>
+          {/* empty div */}
+          <div className="info-link">More Info</div>
+        </div>
+      </>
+    );
+  }
+}
 
 export default Projects;
