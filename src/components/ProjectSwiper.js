362472a296e969559fa08e0c40570378ef3240ec
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2023-02-02 14:51:29 +0000

    Added map function to swiper

--- src/components/ProjectSwiper.js
+++ src/components/ProjectSwiper.js
@@ -1,29 +1,30 @@
-import React, { useRef, useState } from "react";
-
+import React from "react";
+import TestImageData from "../imageData/TestImageData";
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 
 import "../styles/Swiper.css";
-
-// import required modules
 import { Navigation } from "swiper";
 
 export default function ProjectSwiper() {
   return (
     <>
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        {[
+          "/images/test.jpeg",
+          "images/test2.jpeg",
+          "images/test3.jpeg",
+          "images/test4.jpeg",
+          "images/test6.jpeg",
+          "images/paper.jpeg",
+        ].map((i, el) => {
+          return (
+            <SwiperSlide key={i}>
+              <img src={i} alt="" />
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </>
   );
