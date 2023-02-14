9e4cdde2692e3787daedf3d81af28eaaefc71cf3
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2023-02-14 11:50:06 +0000

    Created new layout for work section

--- src/views/Work.js
+++ src/views/Work.js
@@ -6,7 +6,43 @@ import { motion } from "framer-motion";
 const Work = () => {
   return (
     <>
-      <div className="work-container"></div>
+      <div className="work-container">
+        <span>Claudia Cantarini</span>
+        <div className="image-links-container">
+          <img src="images/body.jpeg" alt="" className="image-links" />
+        </div>
+        <div className="image-links-container">
+          <img src="images/fran.jpeg" alt="" className="image-links" />
+        </div>
+        <div className="image-links-container">
+          <img src="images/pearl.jpeg" alt="" className="image-links" />
+        </div>
+        <div className="image-links-container">
+          <img src="images/sophie.jpeg" alt="" className="image-links" />
+        </div>
+
+        <div className="image-links-container">
+          <img src="images/body.jpeg" alt="" className="image-links" />
+        </div>
+        <div className="image-links-container">
+          <img src="images/fran.jpeg" alt="" className="image-links" />
+        </div>
+        <div className="image-links-container">
+          <img src="images/pearl.jpeg" alt="" className="image-links" />
+        </div>
+        <div className="image-links-container">
+          <img src="images/sophie.jpeg" alt="" className="image-links" />
+        </div>
+        <div className="image-links-container">
+          <img src="images/body.jpeg" alt="" className="image-links" />
+        </div>
+        <div className="image-links-container">
+          <img src="images/fran.jpeg" alt="" className="image-links" />
+        </div>
+        <div className="image-links-container">
+          <img src="images/pearl.jpeg" alt="" className="image-links" />
+        </div>
+      </div>
     </>
   );
 };
