eeef843f36fb3c71e5b8a8542e201ffd68b61fc5
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2023-02-23 13:54:41 +0000

    Added function that shows information about project

--- src/views/Projects/Hidden.js
+++ src/views/Projects/Hidden.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import HiddenImg from "../../imageData/HiddenData.js";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css/navigation";
 import "../../styles/Swiper.css";
 import { Navigation } from "swiper";
 
-export default function Hidden() {
+function Hidden() {
+  const [showInfo, setShowInfo] = useState(true);
+
+  const handleShowInfo = () => {
+    setShowInfo(!showInfo);
+  };
+
   return (
     <>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        {HiddenImg.map((el, i) => {
-          return (
-            <SwiperSlide key={i}>
-              <img src={el} alt="" className="swiper-slide-img" />
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
+      {showInfo ? (
+        <>
+          <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+            {HiddenImg.map((el, i) => {
+              return (
+                <SwiperSlide key={i}>
+                  <img src={el} alt="" className="swiper-slide-img" />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+          <div onClick={handleShowInfo}>More info</div>{" "}
+        </>
+      ) : (
+        <div onClick={handleShowInfo}>info here</div>
+      )}
     </>
   );
 }
+
+export default Hidden;
