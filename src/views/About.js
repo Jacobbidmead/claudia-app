a8ef2ccd06f09fa1e3bbde3bcf1b2d31523f4dd8
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2023-03-06 19:04:06 +0000

    Adjusted layout of about view

--- src/views/About.js
+++ src/views/About.js
@@ -5,17 +5,17 @@ import "../styles/About.css";
 const About = () => {
   return (
     <>
+      <div className="links">
+        <span className="top-links">
+          <Link to="/">CLAUDIA CANTARINI</Link>
+        </span>
+        <span className="top-links">
+          <Link to="/Work" className="top-links">
+            Work
+          </Link>
+        </span>
+      </div>
       <div className="about-main-container">
-        <div className="links">
-          <span className="top-links">
-            <Link to="/">CLAUDIA CANTARINI</Link>
-          </span>
-          <span className="top-links">
-            <Link to="/Work" className="top-links">
-              Work
-            </Link>
-          </span>
-        </div>
         <div className="about-container">
           <div className="main-about">
             <div className="about-image-container">
@@ -29,26 +29,26 @@ const About = () => {
                   femininity, focusing on portraiture and the body Her other
                   interest lie in fashion, editorial and lifestyle photography
                 </p>{" "}
+                <span className="about-works">
+                  <b>Selected publications:</b> &bull;Vogue Italia, British
+                  Journal of Photography (Portrait of Humanity, Edition 360) ,
+                  Photo London 2021, Cartier (Saatchi Gallery) Having recently
+                  adopted mixed media art in her practice she has also achieved
+                  a First Class BA in Photography with the installation “All
+                  That Glitters is not Gold”
+                </span>
               </div>
             </div>
-            <span className="about-works">
-              <b>Selected publications:</b> &bull;Vogue Italia, British Journal
-              of Photography (Portrait of Humanity, Edition 360) , Photo London
-              2021, Cartier (Saatchi Gallery) Having recently adopted mixed
-              media art in her practice she has also achieved a First Class BA
-              in Photography with the installation “All That Glitters is not
-              Gold”
-            </span>
           </div>
         </div>
-        <div className="links about-bot-links">
-          <span className="bottom-links ">
-            <Link to="/about">About</Link>
-          </span>
-          <span style={{ color: "black" }} className="bottom-links ">
-            Instagram
-          </span>
-        </div>
+      </div>
+      <div className="links about-bot-links">
+        <span className="bottom-links ">
+          <Link to="/about">About</Link>
+        </span>
+        <span style={{ color: "black" }} className="bottom-links ">
+          Instagram
+        </span>
       </div>
     </>
   );
