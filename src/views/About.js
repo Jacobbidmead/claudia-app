5ebdbbb5b9b8e55b4585e23bc02ec5a6b25f8526
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2023-03-06 19:58:34 +0000

    Adjusted about page so follows the same structure as home page
    - also adjusted responsiveness

--- src/views/About.js
+++ src/views/About.js
@@ -5,50 +5,50 @@ import "../styles/About.css";
 const About = () => {
   return (
     <>
-      <div className="links">
-        <span className="top-links">
-          <Link to="/">CLAUDIA CANTARINI</Link>
-        </span>
-        <span className="top-links">
-          <Link to="/Work" className="top-links">
-            Work
-          </Link>
-        </span>
-      </div>
       <div className="about-main-container">
-        <div className="about-container">
-          <div className="main-about">
-            <div className="about-image-container">
-              <img src="images/Claudia.jpeg" alt="" className="about-image" />{" "}
-              <div className="about-intro">
-                {" "}
-                Claudia is a photographer; born in Italy and raised in London,
-                working with analog and digital photography.{" "}
-                <p>
-                  Claudia’s work is constructed, often based around women and
-                  femininity, focusing on portraiture and the body Her other
-                  interest lie in fashion, editorial and lifestyle photography
-                </p>{" "}
-                <span className="about-works">
-                  <b>Selected publications:</b> &bull;Vogue Italia, British
-                  Journal of Photography (Portrait of Humanity, Edition 360) ,
-                  Photo London 2021, Cartier (Saatchi Gallery) Having recently
-                  adopted mixed media art in her practice she has also achieved
-                  a First Class BA in Photography with the installation “All
-                  That Glitters is not Gold”
-                </span>
-              </div>
-            </div>
+        <div className=" links about-nav">
+          <span className="top-links">
+            <Link to="/">CLAUDIA CANTARINI</Link>
+          </span>
+          <span className="top-links">
+            <Link to="/Work" className="top-links">
+              Work
+            </Link>
+          </span>
+        </div>
+
+        <div className="main-about">
+          <div className="about-image-container">
+            <img src="images/Claudia.jpeg" alt="" className="about-image" />{" "}
+          </div>
+          <div className="about-intro">
+            {" "}
+            Claudia is a photographer; born in Italy and raised in London,
+            working with analog and digital photography.{" "}
+            <p>
+              Claudia’s work is constructed, often based around women and
+              femininity, focusing on portraiture and the body Her other
+              interest lie in fashion, editorial and lifestyle photography
+            </p>{" "}
+            <span className="about-works">
+              <b>Selected publications:</b> &bull;Vogue Italia, British Journal
+              of Photography (Portrait of Humanity, Edition 360) , Photo London
+              2021, Cartier (Saatchi Gallery) Having recently adopted mixed
+              media art in her practice she has also achieved a First Class BA
+              in Photography with the installation “All That Glitters is not
+              Gold”
+            </span>
           </div>
         </div>
-      </div>
-      <div className="links about-bot-links">
-        <span className="bottom-links ">
-          <Link to="/about">About</Link>
-        </span>
-        <span style={{ color: "black" }} className="bottom-links ">
-          Instagram
-        </span>
+
+        <div className="links about-bot-links">
+          <span className="bottom-links ">
+            <Link to="/about">About</Link>
+          </span>
+          <span style={{ color: "black" }} className="bottom-links ">
+            Instagram
+          </span>
+        </div>
       </div>
     </>
   );
