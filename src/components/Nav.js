b4bd4691ee377791c285bffafc67961fb6fbd528
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-08-04 13:26:32 +0100

    Imported links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,26 @@
+import { Link } from "react-router-dom";
+
 const Nav = () => {
   return (
     <>
       <div>
-        <div>Claudia</div>
-        <div>work</div>
-        <div>contact</div>
+        <div>
+          {" "}
+          <Link to="/" style={{ color: "white" }}>
+            Claudia Cantarini
+          </Link>
+        </div>
+        <div>
+          <Link to="/Work" className="top-links" style={{ color: "white" }}>
+            Work
+          </Link>
+        </div>
+        <div>
+          {" "}
+          <Link to="/about" style={{ color: "white" }}>
+            contact
+          </Link>
+        </div>
         <div>instagram</div>
       </div>
     </>
