10a953be0700187c91aa3f287456d39744859593
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-08-04 13:24:34 +0100

    Create Nav bar component

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+const Nav = () => {
+  return (
+    <>
+      <div>
+        <div>Claudia</div>
+        <div>work</div>
+        <div>contact</div>
+        <div>instagram</div>
+      </div>
+    </>
+  );
+};
+
+export default Nav;
