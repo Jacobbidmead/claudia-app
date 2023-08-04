fda14ccd60cf9c039d854f4032e4ed2ca34be531
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-08-04 13:38:20 +0100

    Adjusted nav styling and refactored

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,24 @@
 import { Link } from "react-router-dom";
+import "../styles/Nav.css";
 
 const Nav = () => {
   return (
     <>
-      <div>
+      <div className="nav-bar links">
         <div>
           {" "}
-          <Link to="/" style={{ color: "white" }}>
+          <Link to="/" style={{ color: "black" }}>
             Claudia Cantarini
           </Link>
         </div>
         <div>
-          <Link to="/Work" className="top-links" style={{ color: "white" }}>
+          <Link to="/Work" style={{ color: "black" }}>
             Work
           </Link>
         </div>
         <div>
           {" "}
-          <Link to="/about" style={{ color: "white" }}>
+          <Link to="/about" style={{ color: "black" }}>
             contact
           </Link>
         </div>
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,33 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Nav from "../components/Nav";
 
 const Home = () => {
   return (
     <>
       <div className="home-container">
-        <div className="links">
-          <span className="top-links">
-            <Link to="/" style={{ color: "white" }}>
-              Claudia Cantarini
-            </Link>
-          </span>
-          <span className="top-links">
-            <Link to="/Work" className="top-links" style={{ color: "white" }}>
-              Work
-            </Link>
-          </span>
-        </div>
         <div className="home-image-container">
           <img src="images/home.jpeg" alt="" className="home-image" />
         </div>
-        <div className="links">
-          <span className="bottom-links">
-            <Link to="/about" style={{ color: "white" }}>
-              About
-            </Link>
-          </span>
-          <span className="bottom-links">Instagram</span>
-        </div>
+        <Nav />
       </div>
     </>
   );
