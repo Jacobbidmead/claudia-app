1983a3d63b5cff1196f959087e5aa25011f7aaf3
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-11-20 14:38:24 +0000

    Adjusted styling of home screen

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,8 +6,22 @@ const Home = () => {
     <>
       <div className="home-container">
         <Link to="/Work" className="home-text">
-          <b>CLAUDIA CANTARINI</b>
+          <div className="title">
+            <div>CLAUDIA CANTARINI</div>
+          </div>
         </Link>
+        <div className="home-links">
+          <div>
+            <a
+              href="www.instgram.com"
+              alt=""
+              style={{ color: "white", padding: "10px" }}
+            >
+              insta
+            </a>
+          </div>
+          <div>get in toouch/ </div>
+        </div>
       </div>
     </>
   );
