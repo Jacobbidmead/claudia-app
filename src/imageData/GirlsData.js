830e74da4dd1e3dd32bf88d345b3dea028391a58
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-11-29 12:06:27 +0000

    Removed test girls images

--- src/imageData/GirlsData.js
+++ src/imageData/GirlsData.js
@@ -1,18 +1,3 @@
-let GirlsImg = [
-  "./images/Girls/double.jpg",
-  "images/Girls/girl4.jpg",
-  "images/Girls/girl5.jpg",
-  "images/Girls/girl9.jpg",
-  "images/Girls/girl10.jpg",
-  "images/Girls/girl11.jpg",
-  "images/Girls/girl12.jpg",
-  "images/Girls/girl13.jpg",
-  "images/Girls/girl14.jpg",
-  "images/Girls/girl15.jpg",
-  "images/Girls/girl16.jpg",
-  "images/Girls/girl17.jpg",
-  "images/Girls/girl18.jpg",
-  "images/Girls/girl19.jpg",
-];
+let GirlsImg = ["images/Girls/girl4.jpg"];
 
 export default GirlsImg;
