d1cc744106819fb43cc504d1bf7eb9024879f932
Jacob Bidmead <107250881+Jacobbidmead@users.noreply.github.com>
2023-11-29 14:11:42 +0000

    Added girls images

--- src/imageData/GirlsData.js
+++ src/imageData/GirlsData.js
@@ -1,5 +1,5 @@
 let GirlsImg = [
-  // "images/Girls/girls.jpg",
+  "images/Girls/girls.jpg",
   "images/Girls/girls1.jpg",
   "images/Girls/girls2.jpg",
   "images/Girls/girls4.jpg",
@@ -8,7 +8,7 @@ let GirlsImg = [
   "images/Girls/girls7.jpg",
   "images/Girls/girls8.jpg",
   "images/Girls/girls9.jpg",
-  // "images/Girls/girls10.jpg",
+  "images/Girls/girls10.jpg",
   "images/Models/models2.jpg",
   "images/Models/models3.jpg",
 ];
